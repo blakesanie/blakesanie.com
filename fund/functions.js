46f050589ab4cae8455a19cf8be95e2db1318017
Blake Sanie <blake@sanie.com>
2020-01-02 21:57:54 -0800

    updates and responsive1

--- fund/functions.js
+++ fund/functions.js
@@ -1,167 +1,3 @@
-startingPeriod = 360;
-animationDuration = 4000;
-font = "Dosis";
-
-i = 0;
-initialInterval = setInterval(
-  showFirstNPercentages,
-  animationDuration / startingPeriod
-);
-
-function showFirstNPercentages() {
-  if (i == startingPeriod) {
-    clearInterval(initialInterval);
-    return;
-  }
-  $("tr")
-    .eq(0)
-    .find("td")
-    .eq(1)
-    .text(formatNumbers(myPointsToDisplay[i].y) + "%");
-  $("tr")
-    .eq(1)
-    .find("td")
-    .eq(1)
-    .text(formatNumbers(spyPointsToDisplay[i].y) + "%");
-  i++;
-}
-
-// minY = 0;
-// maxY = 0;
-
-myPointsToDisplay = myPoints.slice(0, startingPeriod);
-spyPointsToDisplay = spyPoints.slice(0, startingPeriod);
-
-var chart = new CanvasJS.Chart("myChart", {
-  animationEnabled: true,
-  animationDuration: animationDuration,
-  backgroundColor: "transparent",
-  toolTip: {
-    enabled: false,
-    shared: true,
-    borderColor: "transparent",
-    fontFamily: font,
-    backgroundColor: "white",
-    cornerRadius: 10,
-    contentFormatter: function(e) {
-      console.log(e);
-      var content = "<div id='myToolTip'>";
-      datasets = ["Sanie", "S&P 500"];
-      console.log(e.entries[0].dataPoint.x);
-      var date = new Date(e.entries[0].dataPoint.x);
-      content +=
-        "<p id='pointDate'>" +
-        (date.getMonth() + 1) +
-        "/" +
-        date.getDate() +
-        "/" +
-        date.getFullYear() +
-        "</p><table id='pointTable' cellspacing='0'>";
-      for (var i = 0; i < e.entries.length; i++) {
-        content +=
-          "<tr><td>" +
-          datasets[i] +
-          "</td><td>" +
-          e.entries[i].dataPoint.y +
-          "%</td></tr>";
-      }
-      return content + "</table></div>";
-    }
-  },
-  axisX: {
-    valueFormatString: "YYYY",
-    labelFontSize: 12,
-    labelFontColor: "white",
-    labelFontFamily: font,
-    lineThickness: 0,
-    tickThickness: 0
-  },
-  axisY2: {
-    gridThickness: 0,
-    minimum: -30,
-    labelFontSize: 12,
-    labelFontColor: "white",
-    labelFontFamily: font,
-    lineThickness: 0,
-    tickThickness: 0,
-    margin: 0,
-    valueFormatString: "###,###,##0",
-    suffix: "%"
-    // interval: 10000
-  },
-  data: [
-    {
-      type: "splineArea",
-      // mousemove: chartHovered,
-      axisYType: "secondary",
-      color: "#ffffff30",
-      lineColor: "white",
-      markerSize: 0,
-      dataPoints: myPointsToDisplay
-    },
-    {
-      type: "splineArea",
-      axisYType: "secondary",
-      color: "black",
-      lineColor: "white",
-      markerSize: 0,
-      dataPoints: spyPointsToDisplay
-    }
-  ]
-});
-chart.render();
-
-var interval;
-setTimeout(function() {
-  interval = setInterval(addDataToGraph, 0);
-}, animationDuration);
-
-var groupSize = 3;
-
-function addDataToGraph() {
-  for (
-    var i = 0;
-    i < groupSize && myPointsToDisplay.length < myPoints.length;
-    i++
-  ) {
-    var currentLength = myPointsToDisplay.length;
-    var newMy = myPoints[currentLength];
-    var newSpy = spyPoints[currentLength];
-    myPointsToDisplay.push(newMy);
-    spyPointsToDisplay.push(newSpy);
-    $("tr")
-      .eq(0)
-      .find("td")
-      .eq(1)
-      .text(formatNumbers(newMy.y) + "%");
-    $("tr")
-      .eq(1)
-      .find("td")
-      .eq(1)
-      .text(formatNumbers(newSpy.y) + "%");
-    // minY = Math.min(newSpy.y, Math.min(newMy.y, minY));
-    // maxY = Math.max(newSpy.y, Math.max(newMy.y, maxY));
-  }
-
-  // moveMarkers();
-  console.log($(window).scrollTop());
-  if (
-    myPointsToDisplay.length == myPoints.length ||
-    $(window).scrollTop() < $(window).height()
-  ) {
-    chart.render();
-    if (myPointsToDisplay.length == myPoints.length) {
-      $(".action").addClass("appearRise");
-      console.log("cancel interval");
-      clearInterval(interval);
-    }
-  }
-}
-
-function formatNumbers(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 $(".info").click(function() {
   overlay = $(this).siblings(".greekOverlay");
   if (overlay.css("opacity") < 1) {
@@ -193,56 +29,155 @@ $(".action").click(function() {
   );
 });
 
-// $(".info").hover(
-//   function() {
-//     $(this)
-//       .siblings(".greekOverlay")
-//       .css("opacity", 1);
-//   },
-//   function() {
-//     $(this)
-//       .siblings(".greekOverlay")
-//       .css("opacity", 0);
-//   }
-// );
+var histogramStep = myHistogramPoints[1].x - myHistogramPoints[0].x;
 
-// setTimeout(function() {
-//   $(".action").addClass("appearRise");
-// }, 10000);
+function removeBeforeDecimal(num) {
+  num = "" + num;
+  if (num.substring(0, 2) == "0.") {
+    return num.substring(1);
+  }
+  if (num.substring(0, 3) == "-0.") {
+    return "-" + num.substring(2);
+  }
+  return num;
+}
 
-// function chartHovered(e) {
-//   x = e.dataPointIndex;
-//   myY = e.dataPoint.y;
-//   spyY = spyPoints[x].y;
-//   // console.log(myY, spyY);
-//   console.log(e);
-//   $("#hoverHelper").css({
-//     display: "block",
-//     transform: "translate(" + e.x + "px," + e.y + "px)"
-//   });
-// }
+var histogram = new CanvasJS.Chart("histogram", {
+  animationEnabled: true,
+  animationDuration: 2000,
+  backgroundColor: "transparent",
+  toolTip: {
+    enabled: true,
+    shared: true,
+    borderColor: "transparent",
+    fontFamily: font,
+    backgroundColor: "white",
+    cornerRadius: 10,
+    contentFormatter: function(e) {
+      var content = "<div id='myToolTip'>";
+      datasets = ["Sanie", "S&P 500"];
+      lower = e.entries[0].dataPoint.x;
+      upper = lower + histogramStep;
+      content +=
+        "<p id='toolTipHeader'>" +
+        removeBeforeDecimal(lower) +
+        "% to " +
+        removeBeforeDecimal(upper) +
+        "%</p><table id='pointTable' cellspacing='0'>";
+      for (var i = 0; i < 2; i++) {
+        factor = i == 1 ? -1 : 1;
+        content +=
+          "<tr><td>" +
+          datasets[i] +
+          "</td><td>" +
+          factor * parseFloat(e.entries[i].dataPoint.y) +
+          "% of weeks</td></tr>";
+      }
+      return content + "</table></div>";
+    }
+  },
+  axisX: {
+    valueFormatString: "#####0",
+    suffix: "%",
+    labelFontSize: 14,
+    labelFontColor: "#00000080",
+    labelFontFamily: font,
+    lineThickness: 0,
+    tickThickness: 0
+    // interval: 2
+  },
+  axisY: {
+    gridThickness: 0,
+    labelFontSize: 0,
+    lineThickness: 0,
+    tickThickness: 0,
+    valueFormatString: "",
+    minimum: -18,
+    maximum: 13
+  },
+  data: [
+    {
+      type: "stackedColumn",
+      color: "#54e4a6",
+      markerSize: 0,
+      dataPoints: myHistogramPoints,
+      lineThickness: 0
+    },
+    {
+      type: "stackedColumn",
+      color: "#9198e5",
+      markerSize: 0,
+      dataPoints: spyHistogramPoints
+    },
+    {
+      type: "splineArea",
+      lineColor: "#54e4a6",
+      color: "#54e4a630",
+      markerSize: 0,
+      dataPoints: myNormalPoints,
+      indexLabelFontSize: 20,
+      indexLabelFontFamily: font,
+      indexLabelPlacement: "outside"
+    },
+    {
+      type: "splineArea",
+      lineColor: "#9198e5",
+      color: "#9198e530",
+      markerSize: 0,
+      dataPoints: spyNormalPoints,
+      indexLabelFontSize: 20,
+      indexLabelFontFamily: font,
+      indexLabelPlacement: "outside"
+    }
+  ]
+});
+histogram.render();
 
-// function moveMarkers() {
-//   var range = maxY - minY;
-//   var currentMyY = myPointsToDisplay[myPointsToDisplay.length - 1].y;
-//   var currentSpyY = spyPointsToDisplay[spyPointsToDisplay.length - 1].y;
-//   var myTranslation = (currentMyY / range) * 10000;
-//   var spyTranslation = (currentSpyY / range) * 10000;
-//   $("#myPercent").css("transform", "translateY(-" + myTranslation + "%)");
-//   $("#spyPercent").css("transform", "translateY(-" + spyTranslation + "%)");
-// }
-
-//{ x: 50, y: 85, indexLabel: "high", markerType: "triangle",markerColor: "red", markerSize: 12 }
-
-// function addDataToGraph() {
-//   if (myPointsToDisplay.length < myPoints.length) {
-//     currentLength = myPointsToDisplay.length;
-//     newLength = Math.min(currentLength + groupSize, myPoints.length);
-//     myPointsToDisplay.push(myPoints.slice(currentLength, newLength));
-//     spyPointsToDisplay.push(spyPoints.slice(currentLength, newLength));
-//     chart.render();
-//   } else {
-//     console.log("cancel interval");
-//     clearInterval(interval);
-//   }
-// }
+var tTest = new CanvasJS.Chart("tTestGraph", {
+  animationEnabled: true,
+  animationDuration: 2000,
+  backgroundColor: "transparent",
+  toolTip: {
+    enabled: false
+  },
+  axisX: {
+    valueFormatString: "#####0.##",
+    suffix: "%",
+    labelFontSize: 0,
+    labelFontColor: "#00000080",
+    labelFontFamily: font,
+    lineThickness: 0,
+    tickThickness: 0
+  },
+  axisY: {
+    gridThickness: 0,
+    labelFontSize: 0,
+    lineThickness: 0,
+    tickThickness: 0,
+    valueFormatString: "",
+    // maximum: 6.5,
+    minimum: -0.7
+  },
+  data: [
+    {
+      type: "splineArea",
+      lineColor: "#9198e5",
+      color: "#9198e540",
+      markerSize: 0,
+      dataPoints: insignificantPoints,
+      indexLabelFontSize: 20,
+      indexLabelFontFamily: font
+    },
+    {
+      type: "splineArea",
+      lineColor: "black",
+      color: "#54e4a6a0",
+      markerSize: 0,
+      dataPoints: significantPoints,
+      indexLabelFontSize: 20,
+      indexLabelFontFamily: font,
+      indexLabelPlacement: "outside"
+    }
+  ]
+});
+tTest.render();
--- fund/parseOutput.py
+++ fund/parseOutput.py
@@ -1,7 +1,12 @@
+import csv
+import numpy as np
+from scipy.stats import norm
+import math
+
 filename = './quantopianOutput.txt'
 
-myReturns = []
-spyReturns = []
+myTotalReturns = []
+spyTotalReturns = []
 dates = []
 
 with open(filename) as file:
@@ -11,26 +16,119 @@ with open(filename) as file:
            rawDate = line.split(" ")[0].split("-")
            dates.append((int(rawDate[0]), int(rawDate[1]), int(rawDate[2])))
            values = line.split(" ")[-1].split(",")
-           print(values)
            myReturn = float(values[0])
            spyReturn = float(values[-1])
-           myReturns.append(myReturn)
-           spyReturns.append(spyReturn)
+           myTotalReturns.append(myReturn)
+           spyTotalReturns.append(spyReturn)
 
-myReturns = [round((item / myReturns[0] - 1) * 100, 1) for item in myReturns]
-spyReturns = [round((item / spyReturns[0] - 1) * 100, 1) for item in spyReturns]
 dates = ["new Date({},{},{})".format(item[0], item[1], item[2]) for item in dates]
+myReturns = [round((item / myTotalReturns[0] - 1) * 100, 1) for item in myTotalReturns]
+spyReturns = [round((item / spyTotalReturns[0] - 1) * 100, 1) for item in spyTotalReturns]
 
 myPoints = ["{{x: {},y: {}}}".format(dates[i], myReturns[i]) for i in range(len(dates))]
 spyPoints = ["{{x: {},y: {}}}".format(dates[i], spyReturns[i]) for i in range(len(dates))]
 
-fileContents = "var myPoints = " + str(myPoints) + ";\n" + "var spyPoints = " + str(spyPoints) + ";"
+fileContents = "var myPoints = " + str(myPoints) + ";\n" + "var spyPoints = " + str(spyPoints) + ";\n"
+
+# statistics
+
+myWeeklyReturns = []
+spyWeeklyReturns = []
+
+for i in range(1, len(myReturns)):
+    try:
+        myWeeklyReturns.append((myTotalReturns[i] - myTotalReturns[i-1]) / myTotalReturns[i-1])
+    except Exception as e:
+        print(e, i)
+    try:
+        spyWeeklyReturns.append((spyTotalReturns[i] - spyTotalReturns[i-1]) / spyTotalReturns[i-1])
+    except Exception as e:
+        print(e, i)
+
+myMean = np.mean(myWeeklyReturns)
+myStd = np.std(myWeeklyReturns)
+
+spyMean = np.mean(spyWeeklyReturns)
+spyStd = np.std(spyWeeklyReturns)
+
+graphRange = (-0.15,0.16)
+numBins = 62
+
+myHistogram = np.histogram(a= myWeeklyReturns, bins = numBins, range=graphRange, density = False)
+spyHistogram = np.histogram(a= spyWeeklyReturns, bins = numBins, range=graphRange, density = False)
+
+# myHistogramPoints = ["{{x: {},y: {}}}".format("{} - {}%".format(int(round(myHistogram[1][i] * 100)), int(round(myHistogram[1][i] * 100)) + 1), spyHistogram[0][i]) for i in range(len(myHistogram[0]))]
+
+myHistogramPoints = ["{{x: {},y: {}}}".format(round(myHistogram[1][i] * 100, 1), round(myHistogram[0][i] / len(myWeeklyReturns) * 100, 2)) for i in range(len(myHistogram[0]))]
+spyHistogramPoints = ["{{x: {},y: {}}}".format(round(spyHistogram[1][i] * 100, 1), -round(spyHistogram[0][i] / len(spyWeeklyReturns) * 100,2)) for i in range(len(spyHistogram[0]))]
+
+print(len(myHistogramPoints))
+
+normalStart = graphRange[0] * 100
+normalEnd = graphRange[1] * 100
+normalInterval = 0.5
+normalScale = len(myWeeklyReturns) * 0.095
+myNormalPoints = norm.pdf(np.arange(normalStart, normalEnd, normalInterval),myMean * 100,myStd * 100)
+myNormalPointsFormatted = []
+meanAdded = False
+for i, y in enumerate(myNormalPoints):
+    x = normalStart + i * normalInterval
+    if x > myMean * 100 and not meanAdded:
+        myNormalPointsFormatted.append("{{x: {},y: {}, indexLabel: \"μ = {}%, σ = {}%\",  markerColor: \"black\", markerSize: 8}}".format(myMean * 100, norm.pdf(myMean * 100,myMean * 100,myStd * 100) * normalScale, round(myMean * 100,2), round(myStd * 100,2)))
+        meanAdded = True
+    x = round(x, 2)
+    myNormalPointsFormatted.append("{{x: {},y: {}}}".format(x, y * normalScale))
+
+# myNormalPointsFormatted = ["{{x: {},y: {}}}".format(round(normalStart + i * normalInterval, 2), myNormalPointsFormatted[i]) for i in range(len(myNormalPointsFormatted))]
+
+# myNormalPointsFormatted.append("{{x: {},y: {}}}".format(myMean * 100, norm.pdf(myMean * 100,myMean * 100,myStd * 100)))
+spyNormalPoints = norm.pdf(np.arange(normalStart, normalEnd, normalInterval),spyMean * 100,spyStd * 100)
+
+spyNormalPointsFormatted = []
+meanAdded = False
+for i, y in enumerate(spyNormalPoints):
+    x = normalStart + i * normalInterval
+    if x > spyMean * 100 and not meanAdded:
+        spyNormalPointsFormatted.append("{{x: {},y: {}, indexLabel: \"μ = {}%, σ = {}%\",  markerColor: \"black\", markerSize: 8}}".format(spyMean * 100, -norm.pdf(spyMean * 100,spyMean * 100,spyStd * 100) * normalScale , round(spyMean * 100,2), round(spyStd * 100,2)))
+        meanAdded = True
+    x = round(x, 2)
+    spyNormalPointsFormatted.append("{{x: {},y: {}}}".format(x, -y * normalScale))
+# spyNormalPointsFormatted = ["{{x: {},y: {}}}".format(round(normalStart + i * normalInterval, 2), spyNormalPointsFormatted[i]) for i in range(len(spyNormalPointsFormatted))]
+meanDiff = (myMean - spyMean)
+stdError = math.sqrt(myStd * myStd + spyStd * spyStd)
+t = meanDiff / stdError * math.sqrt(len(myWeeklyReturns))
+tY = norm.pdf(t,meanDiff,stdError)
+p = 1 - norm.cdf(t, meanDiff, stdError)
+print(p)
+yOffset = 0.5
+width = round(t * 1.4, 2)
+tTestX = np.arange(-0.2, width, 0.02)
+tTestY = norm.pdf(tTestX,meanDiff,stdError)
+insignificantPoints = []
+significantPoints = ["{{x: {},y: {}, }}".format(t, tY + yOffset)]
+for i in range(len(tTestX)):
+    x = round(tTestX[i],2)
+    y = tTestY[i]
+    point = "{{x: {},y: {}}}".format(x, y + yOffset)
+    if x < t:
+        insignificantPoints.append(point)
+    else:
+        significantPoints.append(point)
+insignificantPoints.append("{{x: {},y: {}, indexLabel: \"t = {}, p = {}\",  markerColor: \"black\", markerSize: 8}}".format(t, tY + yOffset, round(t,2), round(p, 7)))
+
+
+
+
+fileContents += "var myNormalPoints = " + str(myNormalPointsFormatted) + ";\n" + "var spyNormalPoints = " + str(spyNormalPointsFormatted) + "; var myHistogramPoints = " + str(myHistogramPoints) + ";\n" + "var spyHistogramPoints = " + str(spyHistogramPoints) + ";\n var insignificantPoints = " + str(insignificantPoints) + ";\n var significantPoints = " + str(significantPoints) + ";"
 
 fileContents = "{".join(fileContents.split("'{"))
 fileContents = "}".join(fileContents.split("}'"))
 
-
-print(fileContents)
-
 with open('data.js', 'w') as file:
     file.write(fileContents)
+
+with open('data.csv', mode='w') as file:
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["S&P 500 Returns", "My Returns"])
+    for i in range(len(myWeeklyReturns)):
+        writer.writerow([spyWeeklyReturns[i], myWeeklyReturns[i]])
