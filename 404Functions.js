6636687b61a6f219795d062bbd63aa1d85e08a0a
Blake Sanie <blake@sanie.com>
2020-04-08 15:23:42 -0700

    cutting down on space

--- 404Functions.js
+++ 404Functions.js
@@ -1,7 +1,7 @@
 var str = "";
 for (var i = 0; i < 10000; i++) {
     if (Math.random() < 0.2) {
-        str += "<span style='opacity: "+ Math.round(Math.random() * 10) / 10 +";'>" + "@#*,.".charAt(Math.floor(Math.random() * 5)) + "</span>"
+        str += "<sp>" + "@#*,.".charAt(Math.floor(Math.random() * 5)) + "</sp>"
     }
     str += "‏‏‎ ‎"
 }
