db7539adc068512d20bac978350f4f8f86662ea7
Blake Sanie <blake@sanie.com>
2020-11-26 21:47:57 -0800

    Updates

--- fun/canvas/functions.js
+++
@@ -1,7 +0,0 @@
-$("canvas").click(function(e) {
-  var posX = $(this).position().left,
-    posY = $(this).position().top;
-  var xPercent = (e.pageX - posX) / $(this).width();
-  var yPercent = (e.pageY - posY) / $(this).height();
-  console.log(xPercent, yPercent);
-});
