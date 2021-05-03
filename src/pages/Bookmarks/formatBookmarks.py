c87fb77888cf61c6d9bc9ff2fc56be71117a0998
Blake Sanie <blake@sanie.com>
2021-05-03 10:39:00 -0400

    automated bookmark updating

--- src/pages/Bookmarks/formatBookmarks.py
+++ src/pages/Bookmarks/formatBookmarks.py
@@ -1,28 +1,40 @@
 import json
 
-with open("./bookmarks.html") as file:
-    text = file.read()
-    file.close()
-
-items = text.split("<DT>")[2:]
-
 objects = []
 
-for i, item in enumerate(items):
-    # print(item)
-    object = {}
-    object["title"] = item[item.index(">") + 1:item.index("</A>")]
-    pairs = item[item.index("<A") + 2: item.index(">")].strip().split(" ")
-    for pair in pairs:
-        splitIndex = pair.index('=')
-        key = pair[:splitIndex]
-        value = pair[splitIndex + 2:-1]
-        if value.isnumeric():
-            value = int(value)
-        object[key] = value
-    objects.append(object)
+with open('./bookmarks.json') as json_file:
+    data = json.load(json_file)
+    for bookmark in data["roots"]["bookmark_bar"]["children"]:
+        print(bookmark)
+        object = {}
+        object["title"] = bookmark["name"]
+        object["HREF"] = bookmark["url"]
+        object["ADD_DATE"] = int(bookmark["date_added"])
+        objects.append(object)
 
-print(objects)
+# with open("./bookmarks.html") as file:
+#     text = file.read()
+#     file.close()
+
+# items = text.split("<DT>")[2:]
+
+# objects = []
+
+# for i, item in enumerate(items):
+#     # print(item)
+#     object = {}
+#     object["title"] = item[item.index(">") + 1:item.index("</A>")]
+#     pairs = item[item.index("<A") + 2: item.index(">")].strip().split(" ")
+#     for pair in pairs:
+#         splitIndex = pair.index('=')
+#         key = pair[:splitIndex]
+#         value = pair[splitIndex + 2:-1]
+#         if value.isnumeric():
+#             value = int(value)
+#         object[key] = value
+#     objects.append(object)
+
+# print(objects)
 
 with open("./bookmarkData.js", "w") as json_file:
     json_file.write("module.exports.bookmarks = {};".format(
