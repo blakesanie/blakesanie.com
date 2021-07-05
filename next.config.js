cfaf0c41e8781c3c8c2ccd726b576ead3b10dd29
Blake Sanie <blake@sanie.com>
2021-07-04 20:37:37 -0700

    explicitly give routes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ module.exports = {
     return {
       "/404.html": { page: "/404" },
       "/index.html": { page: "/" },
+      "/projects": { page: "/projects" },
+      "/photo": { page: "/photo" },
+      "/photo/gear": { page: "/photo/gear" },
+      "/bookmarks": { page: "/bookmarks" },
+      "/resume": { page: "/resume" },
     };
   },
 };
