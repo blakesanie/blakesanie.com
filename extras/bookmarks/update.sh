247934725fa864b5c4b99d01efc3872e588037a5
Blake Sanie <blake@sanie.com>
2021-09-24 13:37:05 -0400

    bookmark update

--- extras/bookmarks/update.sh
+++ extras/bookmarks/update.sh
@@ -1,3 +1,5 @@
 cat /Users/blake/Library/Application\ Support/BraveSoftware/Brave-Browser/Default/Bookmarks > bookmarks.json
 python3 formatBookmarks.py
-npm run deploy
+git add .
+git commit -m "bookmark update"
+git push
