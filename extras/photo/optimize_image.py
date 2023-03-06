7439af1217944f09a2da082719c3d5012eed248b
Blake Sanie <bsanie3@gatech.edu>
2023-03-06 15:46:24 -0500

    centennial image

--- extras/photo/optimize_image.py
+++ extras/photo/optimize_image.py
@@ -3,5 +3,5 @@ import sys
 import os
 
 if __name__ == '__main__':
-    filepath = os.path.join('public', 'images', sys.argv[1])
+    filepath = os.path.join('../../', 'public', 'images', sys.argv[1])
     optimizeImage(filepath)
