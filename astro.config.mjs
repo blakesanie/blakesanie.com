d53a2ee24200dd513488221667f0c6934945e268
Blake Sanie <bsanie3@gatech.edu>
2023-03-13 17:16:14 -0400

    astro-imagetools and astro-icon coming in clutch!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+
+export default {
+  integrations: [astroImageTools],
+};
