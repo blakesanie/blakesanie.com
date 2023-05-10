beeee40e49a7e485abaa85e3ec89b3a6a5d5a806
Blake Sanie <bsanie3@gatech.edu>
2023-05-09 21:07:25 -0700

    trying to make build work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
-import vercelStatic from "@astrojs/vercel/static";
+// import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 // export default defineConfig({});
