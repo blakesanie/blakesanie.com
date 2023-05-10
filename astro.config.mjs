7f1eeb57639161a34259a431ba922b5f97a76edd
Blake Sanie <bsanie3@gatech.edu>
2023-05-09 19:00:43 -0700

    test vercel SSG deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 // export default defineConfig({});
 
-export default {
+// export default {
+
+// integrations: [astroImageTools],
+// };
+
+export default defineConfig({
+  output: "server",
+  adapter: vercelStatic(),
   integrations: [astroImageTools],
-};
+});
---
+++ src/pages/photo/copyPortfolio.py
@@ -0,0 +1,54 @@
+from glob import glob
+from PIL import Image
+import os
+import sys
+from tqdm import tqdm
+import subprocess
+import json
+
+
+if os.path.exists('portfolio'):
+    dir = os.listdir('portfolio')
+    if len(dir) > 0:
+        print('portfolio folder (not empty) already exists')
+        sys.exit(0)
+else:
+    os.mkdir('portfolio')
+
+
+
+def exiftoolFile(filepath):
+    meta = subprocess.check_output([f'exiftool', filepath]).decode('utf-8')
+    md = {}
+    meta.split('\n')
+    for line in meta.split('\n'):
+        parts = line.split(' : ')
+        k = parts[0].strip()
+        v = ' : '.join(parts[1:]).strip()
+        md[k] = v
+    return md
+
+
+
+path = '/Users/blake/Library/Mobile Documents/com~apple~CloudDocs/Images/portfolio'
+
+allMeta = {}
+
+for filepath in tqdm(glob(path + '/*')):
+    img = Image.open(filepath)
+    info = img.info
+    exif = info['exif']
+    filename = filepath.split('/')[-1]
+    img.thumbnail((2000, 2000),Image.ANTIALIAS)
+    newPath = 'portfolio/'+filename
+    img.save(newPath, 'JPEG', quality=100, exif=exif)
+    escapedPath = filepath.replace(' ', '\\ ')
+    escapedNew = newPath.replace(' ', '\\  ')
+    # os.system(f'exiftool -overwrite_original -TagsFromFile {escapedPath} -all:all>all:all {escapedNew}')
+    md = exiftoolFile(filepath)
+    allMeta[filename.split('.')[0]] = md
+    
+with open("metadata.json", "w") as outfile:
+    json.dump(allMeta, outfile)
+    
+    
