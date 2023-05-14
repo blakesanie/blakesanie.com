662e0e678e4d7919f6e3dc4c7062d8bbac5dcd43
Blake Sanie <bsanie3@gatech.edu>
2023-05-13 20:57:08 -0700

    sitemap

--- api/handler.js
+++
@@ -1,9 +0,0 @@
-// import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
-}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import compress from "astro-compress";
+import sitemap from "astro-sitemap";
 // import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
@@ -10,13 +11,24 @@ import compress from "astro-compress";
 //   integrations: [astroImageTools],
 // };
 
+import redirects from "/src/redirects.json";
+const redirectRoutes = new Set(Object.keys(redirects));
+
 export default defineConfig({
   // output: "static",
   // adapter: vercelStatic(),
+  site: "https://blakesanie.com",
   integrations: [
     astroImageTools,
     compress({
       img: false,
+    }),
+    sitemap({
+      filter(page) {
+        const routeFirst = page.split("/")[0];
+        if (redirectRoutes.has(routeFirst)) return false;
+        return true;
+      },
     }),
   ],
 });
