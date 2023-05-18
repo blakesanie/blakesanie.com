51cf72cf850f69cbfc08318de7111d29d768bae6
Blake Sanie <bsanie3@gatech.edu>
2023-05-17 17:47:21 -0700

    trying to figure out hero section

---
+++ public/hero/hero3/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/KDmGPoqNmLmqK9H2/scene.splinecode');
