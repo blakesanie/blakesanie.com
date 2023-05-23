899de0886b7d82ade3547b0417383c1c28a44d30
Blake Sanie <bsanie3@gatech.edu>
2023-05-22 20:52:04 -0700

    clip!

---
+++ src/pages/photo/clip.py
@@ -0,0 +1,34 @@
+from transformers import CLIPProcessor, CLIPModel
+from PIL import Image
+from glob import glob
+from multiprocessing import Pool
+from tqdm import tqdm
+import json
+import torch
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+
+path = '../../assets/images/portfolio'
+
+filepaths = glob(path + '/*')
+    
+print('found files')    
+images = [Image.open(filepath) for filepath in filepaths]
+print('opened images')
+inputs = processor(images=images, return_tensors="pt")
+print('processed images')
+embeddings = model.get_image_features(**inputs)
+embeddings /= torch.norm(embeddings, dim=-1, keepdim=True)
+embeddings = embeddings.detach().numpy().tolist()
+print('extracted embeddings')
+
+out = {}
+
+for filepath, embedding in zip(filepaths, embeddings):
+    name = filepath.split('/')[-1].split('.')[0]
+    out[name] = [ float("{:0.4f}".format(x)) for x in embedding]
+
+with open("../../../public/assets/imageEmbeddings.json", "w") as outfile:
+    json.dump(out, outfile)
