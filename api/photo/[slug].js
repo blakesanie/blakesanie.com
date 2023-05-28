89a52731249acdd6b0d7b734dad52e572b5b647f
Blake Sanie <bsanie3@gatech.edu>
2023-05-28 14:30:37 -0700

    try new api design with image name in route

---
+++ api/photo/[slug].js
@@ -0,0 +1,26 @@
+import { readFileSync } from "fs";
+import path from "path";
+import thumbs from "./.thumbs.js";
+
+export default async function handler(req, res) {
+  const slug = req.query.slug;
+  const split = slug.split("?");
+  const name = split[0];
+  let page = "photo";
+  if (split.length > 1) {
+    page += "/map";
+  }
+  const thumb = thumbs[name];
+  if (!thumb) {
+    return res.redirect("/" + page);
+  }
+  const file = path.join(process.cwd(), "dist", page, "index.html");
+  let str = readFileSync(file, "utf8");
+
+  const cleanName = decodeURIComponent(name.replaceAll("--", "%"));
+  str = str.replaceAll("/og/thumb.jpg", thumb);
+  str = str.replaceAll("Photography |", cleanName + " | Photography |");
+
+  res.setHeader("Content-Type", "text/html");
+  return res.end(str);
+}
--- api/photo/map.js
+++
@@ -1,5 +0,0 @@
-import indexHandler from "./index.js";
-
-export default async function handler(req, res) {
-  return await indexHandler(req, res);
-}
