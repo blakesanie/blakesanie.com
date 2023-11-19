21367c843ead120831ceb2b4e85adf3d8ba2c6e8
Blake Sanie <bsanie3@gatech.edu>
2023-11-18 18:33:22 -0800

    randomize embedding file name to avoid caching

---
+++ src/tooling/randomize_embeddings_file.py
@@ -0,0 +1,21 @@
+import random
+import os
+
+r = str(int(random.random() * 100000000))
+filepath = 'public/assets/imageEmbeddings.json'
+newFilepath = filepath.replace('.', '_' + r + '.')
+
+os.rename(filepath, newFilepath)
+
+pagePath = 'src/pages/photo/index.astro'
+
+with open(pagePath, 'r') as file:
+    file_content = file.read()
+
+# Perform the replacement
+modified_content = file_content.replace('imageEmbeddings.json', f'imageEmbeddings_{r}.json')
+
+# Write the modified content back to the file
+with open(pagePath, 'w') as file:
+    file.write(modified_content)
+    
