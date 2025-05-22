b1a0e60625d64cc3bdde2aefd14baa9e44c5e045
Blake Sanie <blakesanie@gmail.com>
2025-05-21 20:45:49 -0500

    route builder checkpoint

--- next/app/route-builder/page.tsx
+++ next/app/route-builder/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import Map from "./map";
+import { useOverpass } from "./useOverpass";
 import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
@@ -15,38 +19,15 @@ import {
 } from "@/components/ui/sidebar";
 
 export default function Page() {
+  const { fetchCycleways, fetchOneways } = useOverpass();
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
-      </SidebarInset>
+      {/* <SidebarInset> */}
+      <main className="w-full h-screen overflow-hidden flex flex-col">
+        <Map fetchCycleways={fetchCycleways} fetchOneways={fetchOneways} />
+      </main>
+      {/* </SidebarInset> */}
     </SidebarProvider>
   );
 }
