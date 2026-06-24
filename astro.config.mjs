09a89c0ae1c94f63d6d8289bfea52fd8465140fe
Blake Sanie <blakesanie@gmail.com>
2026-06-23 23:11:08 -0500

    image improvements. done for the night

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,22 +22,8 @@ export default defineConfig({
   output: "static",
   // adapter: vercelStatic(),
   site: "https://blakesanie.com",
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: "github-light",
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      lineNumbers: true,
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
   integrations: [
-    // imagePipeline(),
+    imagePipeline(),
     // mdx({
     //   remarkPlugins: [remarkMath],
     //   rehypePlugins: [rehypeKatex],
@@ -68,54 +54,54 @@ export default defineConfig({
         return true;
       },
     }),
-    // compress({
-    //   CSS: {
-    //     csso: {
-    //       comments: false,
-    //       restructure: true,
-    //     },
-    //   },
-    //   HTML: {
-    //     "html-minifier-terser": {
-    //       removeComments: true,
-    //       removeAttributeQuotes: true,
-    //       removeStyleQuotes: true,
-    //       removeScriptTypeAttributes: true,
-    //       removeStyleLinkTypeAttributes: true,
-    //       minifyCSS: true,
-    //       minifyJS: true,
-    //       continueOnParseError: true,
-    //       collapseWhitespace: true,
-    //       collapseBooleanAttributes: true,
-    //     },
-    //   },
-    //   JavaScript: {
-    //     terser: {
-    //       compress: true,
-    //       ie8: false,
-    //       keep_classnames: false,
-    //       keep_fnames: false,
-    //       mangle: true,
-    //       toplevel: true,
-    //     },
-    //   },
-    //   SVG: true,
-    //   Image: false,
-    //   // cssOptions: {
-    //   //   preset: "default", // CSS minification preset
-    //   // },
-    //   // htmlOptions: {
-    //   //   collapseWhitespace: true,
-    //   //   removeComments: true,
-    //   //   minifyCSS: true,
-    //   //   minifyJS: true,
-    //   //   removeAttributeQuotes: true,
-    //   // },
-    //   // jsOptions: {
-    //   //   compress: true,
-    //   //   mangle: true, // Shorten variable names
-    //   // },
-    // }),
+    compress({
+      CSS: {
+        csso: {
+          comments: false,
+          restructure: true,
+        },
+      },
+      HTML: {
+        "html-minifier-terser": {
+          removeComments: true,
+          removeAttributeQuotes: true,
+          removeStyleQuotes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          minifyCSS: true,
+          minifyJS: true,
+          continueOnParseError: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+        },
+      },
+      JavaScript: {
+        terser: {
+          compress: true,
+          ie8: false,
+          keep_classnames: false,
+          keep_fnames: false,
+          mangle: true,
+          toplevel: true,
+        },
+      },
+      SVG: true,
+      Image: false,
+      // cssOptions: {
+      //   preset: "default", // CSS minification preset
+      // },
+      // htmlOptions: {
+      //   collapseWhitespace: true,
+      //   removeComments: true,
+      //   minifyCSS: true,
+      //   minifyJS: true,
+      //   removeAttributeQuotes: true,
+      // },
+      // jsOptions: {
+      //   compress: true,
+      //   mangle: true, // Shorten variable names
+      // },
+    }),
     icon(),
   ],
 });
